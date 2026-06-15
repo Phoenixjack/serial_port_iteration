51b890f94be247896a6c0363a046cf30ddd7191a
PhoenixJack <35715826+Phoenixjack@users.noreply.github.com>
2026-06-14 21:18:51 -0400

    Implement serial data reading in loop
    Add loop to read and print data from serial ports

--- serial_port_iteration.ino
+++ serial_port_iteration.ino
@@ -28,4 +28,21 @@ void setup() {
 }
 
 void loop() {
+  for (uint8_t i = 0; i < io.get_num_ports(); i++) {
+    if (io.newdata(i)) {
+      char c = io.get_char(i);
+
+      Serial.print(F("PORT "));
+      Serial.print(i);
+      Serial.print(F(" RX: "));
+
+      if (c == CHAR_CR) {
+        Serial.println(F("<CR>"));
+      } else if (c == CHAR_NL) {
+        Serial.println(F("<NL>"));
+      } else {
+        Serial.println(c);
+      }
+    }
+  }
 }
